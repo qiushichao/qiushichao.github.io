2ad01a96871c8044ecb7fdf2345351879cb4460c
qiushichao <475345197@qq.com>
2019-03-20 22:41:22 +0800

    Revert "update"
    This reverts commit 90069d1ca4b74e8f69ee5ca154edab91aa3d2361.

--- 3/js/snow_effect.js
+++ 3/js/snow_effect.js
@@ -1,45 +1,25 @@
+
 var piece = new Array(50);
-function showSnow() {
 
-
+for (var i = 0; i < piece.length; i++) {
+    piece[i]= document.createElement("div");
+    piece[i].style.color = "#fff";
+    piece[i].innerHTML = "*";
+    piece[i].style.position = "absolute";
+    piece[i].style.left = Math.ceil(Math.random() * document.body.clientWidth) + "px";
+    piece[i].style.top = Math.random() *document.body.clientHeight + "px";
+    piece[i].style.fontSize=Math.ceil(Math.random()*20) +15+"pt";
+    piece[i].style.weight="bold";
+    document.body.appendChild(piece[i]);
+}
+setInterval(function () {
     for (var i = 0; i < piece.length; i++) {
-        piece[i] = document.createElement("div");
-        piece[i].style.color = "#fff";
-        piece[i].innerHTML = "*";
-        piece[i].style.position = "absolute";
-        piece[i].style.left = Math.ceil(Math.random() * document.body.clientWidth) + "px";
-        piece[i].style.top = Math.random() * document.body.clientHeight + "px";
-        piece[i].style.fontSize = Math.ceil(Math.random() * 20) + 15 + "pt";
-        piece[i].style.weight = "bold";
-        document.body.appendChild(piece[i]);
-    }
-    setInterval(function () {
-        for (var i = 0; i < piece.length; i++) {
-            if (parseInt(piece[i].style.top) < document.body.clientHeight - 100) {
-                piece[i].style.top = parseFloat(piece[i].style.top) + Math.random() * 3 + "px";
-                piece[i].style.left = Math.min(Math.max(0, parseFloat(piece[i].style.left) + Math.random() * 3 - 1.5), document.body.clientWidth - 50) + "px";
-            } else {
-                piece[i].style.left = Math.random() * document.body.clientWidth + "px";
-                piece[i].style.top = 0 + "px";
-            }
+        if (parseInt(piece[i].style.top) < document.body.clientHeight-100) {
+            piece[i].style.top = parseFloat(piece[i].style.top) + Math.random()*3 + "px";
+            piece[i].style.left=Math.min(Math.max(0,parseFloat(piece[i].style.left)+Math.random()*3-1.5),document.body.clientWidth-50) + "px";
+        } else {
+            piece[i].style.left = Math.random() * document.body.clientWidth + "px";
+            piece[i].style.top = 0 + "px";
         }
-    }, 15);
-}
-showSnow();
-function hideSnow() {
-    for (var i = 0; i < piece.length; i++) {
-        piece[i].innerHTML = "";
     }
-}
-function showOrHideSnow() {
-    if(piece[0].innerHTML = ""){
-        for (var i = 0; i < piece.length; i++) {
-            piece[i].innerHTML = "*";
-        }
-        showSnow();
-    }
-    else {
-    
-        hideSnow();
-    }
-}
+}, 15);
